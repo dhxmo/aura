c917bd5fe322abe485796e286625a44751457039
Dhruv Mohapatra <dhrvmohapatra@gmail.com>
2023-12-29 07:32:59 +0530

    check net conn

--- main.py
+++ main.py
@@ -1,43 +1,38 @@
-import os
 import threading
 
 import dearpygui.dearpygui as dpg
+import requests
 
 from core.speech import SpeechRecognitionApp
-from parser.db import create_user
-
-
-def init_db():
-    db_file = "aura.db"
-    if not os.path.exists(db_file):
-        with open(db_file, 'w'): pass
-
-    is_user_create = create_user(db_file="aura.db")
-    if is_user_create:
-        return True
+from parser.db import init_db
 
 
 def init_app():
-    check_db = init_db()
+    init_db()
 
-    if check_db:
-        dpg.create_context()
+    dpg.create_context()
 
-        dpg.create_viewport(title='Aura', width=100, height=100)
-        dpg.setup_dearpygui()
+    dpg.create_viewport(title='Aura', width=100, height=100)
+    dpg.setup_dearpygui()
 
-        # TODO: check internet connection. If no internet, give error. Else
-
-        # TODO: check if update published. if yes, ask to update
+    # Check internet connection
+    timeout = 1
+    try:
+        requests.head("http://www.google.com/", timeout=timeout)
 
         # Create a thread for recognizing speech
         sr = SpeechRecognitionApp()
         speech_thread = threading.Thread(target=sr.run)
         speech_thread.start()
 
-        dpg.show_viewport()
-        dpg.start_dearpygui()
-        dpg.destroy_context()
+    except requests.ConnectionError:
+        # Display error message in DearPyGUI viewport
+        with dpg.window(label="Error"):
+            dpg.add_text("The internet connection is down. Please reconnect and restart this app")
+
+    dpg.show_viewport()
+    dpg.start_dearpygui()
+    dpg.destroy_context()
 
 
 if __name__ == '__main__':
--- parser/db.py
+++ parser/db.py
@@ -1,27 +1,38 @@
 import hashlib
 import logging
+import os
 import sqlite3
 
 
-def create_user(db_file):
-    user_id = hashlib.sha256(str(id).encode()).hexdigest()
+def init_db():
+    db_file = "aura.db"
+    if not os.path.exists(db_file):
+        with open(db_file, 'w'): pass
+
     try:
         conn = sqlite3.connect(db_file)
         c = conn.cursor()
 
-        # Modify the CREATE TABLE statement to use INTEGER PRIMARY KEY
-        c.execute("CREATE TABLE IF NOT EXISTS assistants (id INTEGER PRIMARY KEY, user_id TEXT, assistant_id TEXT, "
-                  "thread_id TEXT)")
+        # create a new table if nothing exists
+        c.execute(
+            "CREATE TABLE IF NOT EXISTS assistants (id INTEGER PRIMARY KEY, user_id TEXT, assistant_id TEXT, "
+            "thread_id TEXT)")
 
-        # Remove the id from the INSERT statement since it's now auto-generated
-        c.execute("INSERT INTO assistants (user_id) VALUES (?)", (user_id,))
+        # select user row
+        c.execute("SELECT * FROM assistants LIMIT 1")
+        user = c.fetchone()
 
-        conn.commit()
-        conn.close()
+        # if user doesnt exist, add a new user
+        if not user:
+            user_id = hashlib.sha256(str(id).encode()).hexdigest()
 
-        return True
+            # Remove the id from the INSERT statement since it's now auto-generated
+            c.execute("INSERT INTO assistants (user_id) VALUES (?)", (user_id,))
+
+            conn.commit()
+            conn.close()
     except Exception as e:
-        logging.info("Error occurred while create_user: ", str(e))
+        logging.info("Error occurred while init_db: ", str(e))
 
 
 def create_assistant_id(user_id, assistant_id, thread_id, db_file):
